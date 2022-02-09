1fd9abf75819cd133cb3bbcedd98e88f0f583e3c
VolodyaBorshch <44494679+VolodyaBorshch@users.noreply.github.com>
2022-02-09 14:40:09 +0200

    0.03

---
+++ Simulator Marketologa/Assets/Script/OptionMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolime(float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void Sound()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+}
